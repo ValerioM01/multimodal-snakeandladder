1c6fe808f1b9071ea4153049498b93dcc3bb6068
Valerio Mesiti <80749762+ValerioM01@users.noreply.github.com>
2026-06-03 14:32:45 +0200

    Delete Constants.kt

--- Constants.kt
+++
@@ -1,7 +0,0 @@
-package com.example.snakesandladders.utils
-
-object Constants {
-    const val TurnAnnouncementDelayMs = 1200L
-    const val StepAnimationDelayMs = 350L
-    const val SnakeLadderAnimationDelayMs = 420L
-}
