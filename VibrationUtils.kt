87675900393ab5ab3d40cdca44848f33db0f02b1
Valerio Mesiti <80749762+ValerioM01@users.noreply.github.com>
2026-06-03 14:34:02 +0200

    Delete VibrationUtils.kt

--- VibrationUtils.kt
+++
@@ -1,7 +0,0 @@
-package com.example.snakesandladders.utils
-
-import android.content.Context
-
-fun vibrateDevice(context: Context, pattern: LongArray, repeat: Int = -1) {
-    // Move the original vibration implementation here.
-}
