110102a58a9f8ea0df85b09c4f43412c7801c59f
Valerio Mesiti <80749762+ValerioM01@users.noreply.github.com>
2026-06-03 14:33:24 +0200

    Delete HomeScreen.kt

--- HomeScreen.kt
+++
@@ -1,9 +0,0 @@
-package com.example.snakesandladders.ui.screens
-
-import androidx.compose.runtime.Composable
-
-@Composable
-fun HomeScreen(onStartClick: () -> Unit) {
-    // Move the original HomeScreen UI here.
-    // Keep this screen focused on rendering only.
-}
